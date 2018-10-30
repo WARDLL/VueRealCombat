1398ceb964578ecd4f085f1931460281464a4436
WARDLL <lhwardll@gmail.com>
2018-10-30 21:19:09 +0800

    list完成

---
+++ src/pages/detail/components/List.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <div class="item" v-for="(item,index) of list" :key="index">
+            <div class="item-title border-bottom">
+                <span class="item-title-icon"></span>
+                {{item.title}}
+            </div>
+            <!-- 多级菜单 -->
+            <div v-if="item.children" class="item-chilren">
+                <!-- 递归组件 -->
+                <!-- 如果有children，就把children当成list再传给自己 -->
+                <detail-list :list="item.children"></detail-list>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailList',
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.item-title
+    line-height 0.8rem
+    font-size 0.32rem
+    padding 0 0.2rem
+    .item-title-icon
+        position relative
+        left 0.1rem
+        top 0.08rem
+        display inline-block
+        width 0.36rem
+        height 0.36rem
+        background url('http://s.qunarzz.com/piao/image/touch/sight/detail.png') 0 -0.45rem no-repeat // s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat;
+        margin-right 0.1rem
+        background-size 0.4rem 3rem
+.item-chilren
+    padding 0 0.3rem
+</style>
